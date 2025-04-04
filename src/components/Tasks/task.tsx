780576546526248523861dee5bec3b414642ef2b
allanvinicius <allanviniciosdemoura@gmail.com>
2025-04-04 10:37:21 -0300

    fix

--- src/components/Tasks/task.tsx
+++ src/components/Tasks/task.tsx
@@ -1,12 +1,9 @@
 import { TaskList } from "./task-list";
-import { Container } from "@/components/Container";
 
 export function Task() {
   return (
     <section className="w-full flex flex-col">
-      <Container>
-        <TaskList />
-      </Container>
+      <TaskList />
     </section>
   );
 }
